96e8173e45b196aeb647533f2d72f9f89de9ff18
Luigi de Albuquerque <dealbuquerqueluigi@gmail.com>
2022-10-10 18:33:52 -0300

    Criação de servico-prestado-lista

--- src/app/servico-prestado.service.ts
+++ src/app/servico-prestado.service.ts
@@ -1,8 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ServicoPrestado } from './servico-prestado/servicoPrestado';
 import { environment } from '../environments/environment'
+import { ServicoPrestadoBusca } from './servico-prestado/servico-prestado-lista/servicoPrestadoBusca';
+import { url } from 'inspector';
 
 @Injectable({
   providedIn: 'root'
@@ -16,9 +18,20 @@ export class ServicoPrestadoService {
   }
 
   salvar(servicoPrestado: ServicoPrestado): Observable<ServicoPrestado> {
-    return this.http.post<ServicoPrestado>(`${this.apiURL}`, ServicoPrestado);
+    return this.http.post<ServicoPrestado>(`${this.apiURL}`, servicoPrestado);
 
   }
+
+  buscar(nome: string, mes: number) : Observable<ServicoPrestadoBusca[]>{
+
+    const httpParams = new HttpParams()
+    .set("nome", nome)
+    .set("mes", mes.toString());
+   
+    const url = this.apiURL + "?" + httpParams.toString();
+    return this.http.get<any>(url)
+  }
+
 }
 
 
